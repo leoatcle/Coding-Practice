4e15f848d67f388c119aa0c929f6e6f6b69ac357
leoatcle <mxz132@case.edu>
2013-01-15 23:25:51 -0500

    customized port, hyperlink to file

--- WebServer.rb
+++ WebServer.rb
@@ -1,19 +1,37 @@
 require 'socket'
 
-server = TCPServer.new(5000)
-loop do
-  client = server.accept
-  client.print "PATH OF CURRENT DIRECTORY: \t"
-  client.puts File.dirname(__FILE__)
+begin
+  print "Please enter the desired port number: "
+  input = gets.chomp
+  port = /\d/.match(input)? input.to_i : 4000
+  server = TCPServer.new(port)
+rescue 
+  retry
+end
 
-  Dir.glob("*").each do |f|
-    client.printf "%s \t %s \n", f, File.stat(f).ctime
-  end
+begin
+  loop do
+    client = server.accept  
+    
+    while line=client.gets
+      puts line.chop
+      break if line =~ /^\s*$/
+    end
+      
+    client.puts "<html>"
+    client.puts "<head>PATH OF CURRENT DIRECTORY: \t<br /></head><body>"
+  
+    Dir.glob("*").each do |f|
+      client.puts "<a href='"+f+"'>"+f + "</a>\t" + File.stat(f).ctime.to_s+"<br />"
+    end
+  
+    client.puts "</body></html>"
              
-  while line=client.gets
-    puts line.chop
-    break if line =~ /^\s*$/
+    client.close
   end
-  client.close
+rescue => e
+  puts e.message
+  server = TCPServer.new(port)
+  retry
 end
 
